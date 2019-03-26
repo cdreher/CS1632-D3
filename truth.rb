573f9774390dfc6d65f626f2ff289a23d96a47ad
Collin Dreher <collin.dreher@gmail.com>
2019-03-26 17:54:54 -0400

    Show display results and error page

--- truth.rb
+++ truth.rb
@@ -5,3 +5,57 @@ require 'sinatra/reloader'
 get '/' do
   erb :index
 end
+
+# Display results
+get '/display' do
+  # Display error message
+  if params.length != 3 or params[:true] == nil or params[:false] == nil or params[:size] == nil
+    status 404
+    return erb :error
+  end
+
+  # IF args are empty, assign default values
+  if params[:true] == ''
+    params[:true] = 'T'
+  end
+  if params[:false] == ''
+    params[:false] = 'F'
+  end
+  if params[:size] == ''
+    params[:size] = '3'
+  end
+
+  table = Array.new
+  n = params[:size].to_i
+
+  # Start loop
+  (2**n).times do |i|
+    expression = Array.new
+    logical_AND = true
+    logical_OR = false
+
+    n.times do |j|
+      if((i/(2**(n-j-1)))%2 == 1)
+				logical_OR = true
+				expression << true
+			else
+				logical_AND = false
+				expression << false
+			end
+		end
+		expression << logical_AND
+		expression << logical_OR
+		expression << !logical_AND
+    expression << !logical_OR
+		table << expression
+  end
+
+  # Display proper results
+  erb :display, :locals => { trueSymbol: params[:true], falseSymbol: params[:false], size: params[:size], table: table}
+end
+
+# If we have a URL other than one specified
+not_found do
+  status 404
+  erb :error
+end
